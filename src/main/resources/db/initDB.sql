6257f33194b84bc25c9f74a47858e0fd63bfe159
LenaLaRoux <lena.laroux@gmail.com>
2025-02-23 20:32:51 +0500

    Change 3_12_db_implementation

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,21 @@
+CREATE SEQUENCE IF NOT EXISTS global_seq START WITH 100000;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    name             VARCHAR                           NOT NULL,
+    email            VARCHAR                           NOT NULL,
+    password         VARCHAR                           NOT NULL,
+    registered       TIMESTAMP           DEFAULT now() NOT NULL,
+    enabled          BOOL                DEFAULT TRUE  NOT NULL,
+    calories_per_day INTEGER             DEFAULT 2000  NOT NULL
+);
+CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE IF NOT EXISTS user_role
+(
+    user_id INTEGER NOT NULL,
+    role    VARCHAR NOT NULL,
+    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
